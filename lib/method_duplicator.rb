c553c114a75d28708a5238750f2e4811ef79b9cf
Théo Dugautier <theo@hoggo.com>
2022-12-01 14:54:21 +0100

    Fix few same methods in one line counter

--- lib/method_duplicator.rb
+++ lib/method_duplicator.rb
@@ -6,15 +6,22 @@ require "method_duplicator/method_format"
 
 module MethodDuplicator
   def self.get_unused_method
+    # Get all methods arrays
     files_list = FilesWrapper.new.list
     methods_array = MethodsWrapper.new(files: files_list).get_all_methods_from_file_array
+
 
     files_list.each_with_index do |file, index|
       yield((index * 100) / files_list.count)
       File.open(file).each do |line|
+        new_line = line.gsub(/#.*/, '#')
         methods_array.each do |item|
-          if line.include?("#{item.name}")
-            item.use_counter += 1
+          # counter le nombre de fois ou apparait le name
+          if new_line.include?(item.name)
+            # incrementer le counter
+            item.use_counter += new_line.split(/(\.| )/).count do |word|
+              word.include?(item.name)
+            end
           end
         end
       end
--- test/dummy/app/jobs/application_job.rb
+++ test/dummy/app/jobs/application_job.rb
@@ -4,4 +4,9 @@ class ApplicationJob < ActiveJob::Base
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
+
+  # TODO: age
+  def testing
+    User.age == 12 && User.age == 14
+  end
 end
